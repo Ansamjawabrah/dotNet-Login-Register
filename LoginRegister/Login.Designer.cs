352ecc65b339166898139cc2e780a75930ed62d5
unknown <nasoomjawabrah100@gmail.com>
2021-07-29 14:10:11 +0300

    updating

--- LoginRegister/Login.Designer.cs
+++ LoginRegister/Login.Designer.cs
@@ -41,17 +41,19 @@ namespace LoginRegister
             this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
             this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
             this.guna2CircleButton3 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picbox1)).BeginInit();
             this.SuspendLayout();
             // 
             // picbox1
             // 
             this.picbox1.Image = ((System.Drawing.Image)(resources.GetObject("picbox1.Image")));
-            this.picbox1.Location = new System.Drawing.Point(208, 90);
-            this.picbox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picbox1.Location = new System.Drawing.Point(156, 73);
+            this.picbox1.Margin = new System.Windows.Forms.Padding(2);
             this.picbox1.Name = "picbox1";
             this.picbox1.ShadowDecoration.Parent = this.picbox1;
-            this.picbox1.Size = new System.Drawing.Size(409, 484);
+            this.picbox1.Size = new System.Drawing.Size(307, 393);
             this.picbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picbox1.TabIndex = 0;
             this.picbox1.TabStop = false;
@@ -62,10 +64,10 @@ namespace LoginRegister
             this.Label1.BackColor = System.Drawing.Color.Transparent;
             this.Label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
-            this.Label1.Location = new System.Drawing.Point(715, 90);
-            this.Label1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Label1.Location = new System.Drawing.Point(536, 73);
+            this.Label1.Margin = new System.Windows.Forms.Padding(2);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(182, 29);
+            this.Label1.Size = new System.Drawing.Size(136, 24);
             this.Label1.TabIndex = 1;
             this.Label1.Text = "Welcome back!";
             // 
@@ -74,7 +76,7 @@ namespace LoginRegister
             this.UsernameTB.Animated = true;
             this.UsernameTB.AutoRoundedCorners = true;
             this.UsernameTB.BorderColor = System.Drawing.SystemColors.ControlDark;
-            this.UsernameTB.BorderRadius = 28;
+            this.UsernameTB.BorderRadius = 22;
             this.UsernameTB.BorderThickness = 2;
             this.UsernameTB.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.UsernameTB.DefaultText = "";
@@ -90,23 +92,24 @@ namespace LoginRegister
             this.UsernameTB.HoverState.Parent = this.UsernameTB;
             this.UsernameTB.IconLeft = ((System.Drawing.Image)(resources.GetObject("UsernameTB.IconLeft")));
             this.UsernameTB.IconLeftOffset = new System.Drawing.Point(5, 0);
-            this.UsernameTB.Location = new System.Drawing.Point(640, 150);
-            this.UsernameTB.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.UsernameTB.Location = new System.Drawing.Point(480, 122);
+            this.UsernameTB.Margin = new System.Windows.Forms.Padding(6);
             this.UsernameTB.Name = "UsernameTB";
             this.UsernameTB.PasswordChar = '\0';
             this.UsernameTB.PlaceholderForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.UsernameTB.PlaceholderText = "Email";
             this.UsernameTB.SelectedText = "";
             this.UsernameTB.ShadowDecoration.Parent = this.UsernameTB;
-            this.UsernameTB.Size = new System.Drawing.Size(332, 58);
+            this.UsernameTB.Size = new System.Drawing.Size(249, 47);
             this.UsernameTB.TabIndex = 2;
+            this.UsernameTB.TextChanged += new System.EventHandler(this.UsernameTB_TextChanged);
             // 
             // PasswordTB
             // 
             this.PasswordTB.Animated = true;
             this.PasswordTB.AutoRoundedCorners = true;
             this.PasswordTB.BorderColor = System.Drawing.SystemColors.ControlDark;
-            this.PasswordTB.BorderRadius = 28;
+            this.PasswordTB.BorderRadius = 22;
             this.PasswordTB.BorderThickness = 2;
             this.PasswordTB.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.PasswordTB.DefaultText = "";
@@ -122,15 +125,15 @@ namespace LoginRegister
             this.PasswordTB.HoverState.Parent = this.PasswordTB;
             this.PasswordTB.IconLeft = ((System.Drawing.Image)(resources.GetObject("PasswordTB.IconLeft")));
             this.PasswordTB.IconLeftOffset = new System.Drawing.Point(5, 0);
-            this.PasswordTB.Location = new System.Drawing.Point(640, 222);
-            this.PasswordTB.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.PasswordTB.Location = new System.Drawing.Point(480, 180);
+            this.PasswordTB.Margin = new System.Windows.Forms.Padding(6);
             this.PasswordTB.Name = "PasswordTB";
             this.PasswordTB.PasswordChar = '\0';
             this.PasswordTB.PlaceholderForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.PasswordTB.PlaceholderText = "Password";
             this.PasswordTB.SelectedText = "";
             this.PasswordTB.ShadowDecoration.Parent = this.PasswordTB;
-            this.PasswordTB.Size = new System.Drawing.Size(332, 58);
+            this.PasswordTB.Size = new System.Drawing.Size(249, 47);
             this.PasswordTB.TabIndex = 2;
             this.PasswordTB.UseSystemPasswordChar = true;
             // 
@@ -140,10 +143,10 @@ namespace LoginRegister
             this.lb2.BackColor = System.Drawing.Color.Transparent;
             this.lb2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
-            this.lb2.Location = new System.Drawing.Point(851, 289);
-            this.lb2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lb2.Location = new System.Drawing.Point(638, 235);
+            this.lb2.Margin = new System.Windows.Forms.Padding(2);
             this.lb2.Name = "lb2";
-            this.lb2.Size = new System.Drawing.Size(121, 21);
+            this.lb2.Size = new System.Drawing.Size(91, 17);
             this.lb2.TabIndex = 1;
             this.lb2.Text = "Forgot Password?";
             // 
@@ -151,7 +154,7 @@ namespace LoginRegister
             // 
             this.loginbtn.Animated = true;
             this.loginbtn.AutoRoundedCorners = true;
-            this.loginbtn.BorderRadius = 28;
+            this.loginbtn.BorderRadius = 22;
             this.loginbtn.CheckedState.Parent = this.loginbtn;
             this.loginbtn.CustomImages.Parent = this.loginbtn;
             this.loginbtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(60)))), ((int)(((byte)(40)))));
@@ -159,23 +162,23 @@ namespace LoginRegister
             this.loginbtn.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginbtn.ForeColor = System.Drawing.Color.White;
             this.loginbtn.HoverState.Parent = this.loginbtn;
-            this.loginbtn.Location = new System.Drawing.Point(711, 326);
-            this.loginbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginbtn.Location = new System.Drawing.Point(533, 265);
             this.loginbtn.Name = "loginbtn";
             this.loginbtn.ShadowDecoration.Parent = this.loginbtn;
-            this.loginbtn.Size = new System.Drawing.Size(191, 58);
+            this.loginbtn.Size = new System.Drawing.Size(143, 47);
             this.loginbtn.TabIndex = 4;
             this.loginbtn.Text = "Login";
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
             // 
             // guna2HtmlLabel1
             // 
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(737, 411);
-            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(553, 334);
+            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(2);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(139, 21);
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(109, 18);
             this.guna2HtmlLabel1.TabIndex = 1;
             this.guna2HtmlLabel1.Text = "- Or connect using -";
             // 
@@ -185,10 +188,10 @@ namespace LoginRegister
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(693, 551);
-            this.guna2HtmlLabel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(520, 448);
+            this.guna2HtmlLabel2.Margin = new System.Windows.Forms.Padding(2);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            this.guna2HtmlLabel2.Size = new System.Drawing.Size(226, 21);
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(170, 17);
             this.guna2HtmlLabel2.TabIndex = 1;
             this.guna2HtmlLabel2.Text = "Dont have an account? Sign Up";
             // 
@@ -203,12 +206,11 @@ namespace LoginRegister
             this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
             this.guna2CircleButton1.HoverState.Parent = this.guna2CircleButton1;
             this.guna2CircleButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButton1.Image")));
-            this.guna2CircleButton1.Location = new System.Drawing.Point(678, 452);
-            this.guna2CircleButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2CircleButton1.Location = new System.Drawing.Point(508, 367);
             this.guna2CircleButton1.Name = "guna2CircleButton1";
             this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CircleButton1.ShadowDecoration.Parent = this.guna2CircleButton1;
-            this.guna2CircleButton1.Size = new System.Drawing.Size(80, 74);
+            this.guna2CircleButton1.Size = new System.Drawing.Size(60, 60);
             this.guna2CircleButton1.TabIndex = 5;
             // 
             // guna2CircleButton2
@@ -222,12 +224,11 @@ namespace LoginRegister
             this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
             this.guna2CircleButton2.HoverState.Parent = this.guna2CircleButton2;
             this.guna2CircleButton2.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButton2.Image")));
-            this.guna2CircleButton2.Location = new System.Drawing.Point(766, 452);
-            this.guna2CircleButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2CircleButton2.Location = new System.Drawing.Point(574, 367);
             this.guna2CircleButton2.Name = "guna2CircleButton2";
             this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CircleButton2.ShadowDecoration.Parent = this.guna2CircleButton2;
-            this.guna2CircleButton2.Size = new System.Drawing.Size(80, 74);
+            this.guna2CircleButton2.Size = new System.Drawing.Size(60, 60);
             this.guna2CircleButton2.TabIndex = 5;
             // 
             // guna2CircleButton3
@@ -241,20 +242,39 @@ namespace LoginRegister
             this.guna2CircleButton3.ForeColor = System.Drawing.Color.White;
             this.guna2CircleButton3.HoverState.Parent = this.guna2CircleButton3;
             this.guna2CircleButton3.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButton3.Image")));
-            this.guna2CircleButton3.Location = new System.Drawing.Point(854, 452);
-            this.guna2CircleButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2CircleButton3.Location = new System.Drawing.Point(640, 367);
             this.guna2CircleButton3.Name = "guna2CircleButton3";
             this.guna2CircleButton3.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             this.guna2CircleButton3.ShadowDecoration.Parent = this.guna2CircleButton3;
-            this.guna2CircleButton3.Size = new System.Drawing.Size(80, 74);
+            this.guna2CircleButton3.Size = new System.Drawing.Size(60, 60);
             this.guna2CircleButton3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(533, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1149, 719);
+            this.ClientSize = new System.Drawing.Size(862, 584);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.guna2CircleButton3);
             this.Controls.Add(this.guna2CircleButton2);
             this.Controls.Add(this.guna2CircleButton1);
@@ -267,13 +287,14 @@ namespace LoginRegister
             this.Controls.Add(this.Label1);
             this.Controls.Add(this.picbox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Log In";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picbox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -293,6 +314,8 @@ namespace LoginRegister
         private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
         private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
         private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- LoginRegister/Login.cs
+++ LoginRegister/Login.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 namespace LoginRegister
 {
     public partial class Login : Form
@@ -16,5 +18,75 @@ namespace LoginRegister
         {
             InitializeComponent();
         }
+        IFirebaseConfig conf = new FirebaseConfig()
+        {
+            AuthSecret = "0v7BZ4bmbNv1wi424GVkriuhARxSU72n23oXym0Y",
+            BasePath = "https://login-43b1b-default-rtdb.firebaseio.com/"
+
+        };
+        IFirebaseClient cli;
+        private void Login_Load(object sender, EventArgs e)
+        {
+            textBox1.Hide();
+            button1.Hide();
+            try
+            {
+             cli = new FireSharp.FirebaseClient(conf);
+
+            }catch (Exception ex)
+            {
+            MessageBox.Show ("there has been an error");
+            }
+        }
+
+        private void UsernameTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+          User u = new User()
+            {   Email = UsernameTB.Text,
+                UserName = textBox1.Text,
+                Password = PasswordTB.Text
+
+            };
+            cli.Set("Users/" + UsernameTB.Text.ToString(), u);
+
+        }
+
+
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                User user = cli.Get("Users/" + UsernameTB.Text).ResultAs<User>();
+                if (user != null)
+                {
+                    if (user.Password == PasswordTB.Text)
+                    {
+                        MessageBox.Show("Hello" + " " + user.UserName);
+                        
+                    }
+                    else MessageBox.Show("Wrong Password ");
+                }
+                else MessageBox.Show("There is no such a user with this name ! \n");
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Check your counction !");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
     }
+    
 }
+
